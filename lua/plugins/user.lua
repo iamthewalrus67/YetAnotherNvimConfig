6474dc371e4d0a26956eda306c7d6e0e6bbc5908
Bohdan Ruban <bohdan.ruban02@gmail.com>
2024-04-05 12:29:36 +0300

    Initial commit

---
+++ lua/plugins/user.lua
@@ -0,0 +1,43 @@
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- Here are some examples:
+
+---@type LazySpec
+return {
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^4', -- Recommended
+    ft = { 'rust' },
+  },
+
+  -- == Examples of Overriding Plugins ==
+
+  -- customize alpha options
+  {
+    "goolord/alpha-nvim",
+    opts = function(_, opts)
+      -- customize the dashboard header
+      opts.section.header.val = {
+"                            .                    ",
+"    ##############..... ##############   ",
+"    ##############......##############   ",
+"      ##########..........##########     ",
+"      ##########........##########       ",
+"      ##########.......##########        ",
+"      ##########.....##########..        ",
+"      ##########....##########.....      ",
+"    ..##########..##########.........    ",
+"  ....##########.#########.............  ",
+"    ..################JJJ............    ",
+"      ################.............      ",
+"      ##############.JJJ.JJJJJJJJJJ      ",
+"      ############...JJ...JJ..JJ  JJ     ",
+"      ##########....JJ...JJ..JJ  JJ      ",
+"      ########......JJJ..JJJ JJJ JJJ     ",
+"      ######    .........                ",
+"                  .....                  ",
+"                    .                    ",
+      }
+      return opts
+    end,
+  },
+}
